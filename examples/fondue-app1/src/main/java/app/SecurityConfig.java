c2677986e78fa4685356e6c48c20951d3c6363ec
argius <argius.net@gmail.com>
2019-10-30 23:24:23 +0900

    Invalidate app.SecurityConfig (bad condition on clean-up project)

--- examples/fondue-app1/src/main/java/app/SecurityConfig.java
+++ examples/fondue-app1/src/main/java/app/SecurityConfig.java
@@ -1,114 +1,110 @@
 package app;
 
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.validation.MessageCodesResolver;
-import org.springframework.validation.Validator;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-    }
-
-    @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
-    }
-
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-    }
-
-    @Override
-    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
-    }
-
-    @Override
-    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-    }
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-    }
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-    }
-
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry) {
-    }
-
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-    }
-
-    @Override
-    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
-    }
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-    }
-
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-    }
-
-    @Override
-    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
-    }
-
-    @Override
-    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
-    }
-
-    @Override
-    public Validator getValidator() {
-        return null;
-    }
-
-    @Override
-    public MessageCodesResolver getMessageCodesResolver() {
-        return null;
-    }
-}
+//import java.util.List;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.format.FormatterRegistry;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.validation.MessageCodesResolver;
+//import org.springframework.validation.Validator;
+//import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+//import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+//import org.springframework.web.servlet.HandlerExceptionResolver;
+//import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+//import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+//
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//    }
+//
+//    @Override
+//    public void configurePathMatch(PathMatchConfigurer configurer) {
+//    }
+//
+//    @Override
+//    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//    }
+//
+//    @Override
+//    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+//    }
+//
+//    @Override
+//    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+//    }
+//
+//    @Override
+//    public void addFormatters(FormatterRegistry registry) {
+//    }
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//    }
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//    }
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//    }
+//
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//    }
+//
+//    @Override
+//    public void configureViewResolvers(ViewResolverRegistry registry) {
+//    }
+//
+//    @Override
+//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+//    }
+//
+//    @Override
+//    public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
+//    }
+//
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//    }
+//
+//    @Override
+//    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//    }
+//
+//    @Override
+//    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+//    }
+//
+//    @Override
+//    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+//    }
+//
+//    @Override
+//    public Validator getValidator() {
+//        return null;
+//    }
+//
+//    @Override
+//    public MessageCodesResolver getMessageCodesResolver() {
+//        return null;
+//    }
+//}
