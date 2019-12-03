555fadc11a0466e064decc58b016c3e0dd0cc61e
argius <argius.net@gmail.com>
2019-12-04 00:22:44 +0900

    Remove unnecessary function 'actSessionAvailableOrNot()' in every common.js

--- framework/src/main/resources/fondue/init/common.js
+++ framework/src/main/resources/fondue/init/common.js
@@ -11,25 +11,6 @@ function isMobile() {
 	return (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
 }
 
-function actSessionAvailableOrNot(okAction, ngAction, errorAction) {
-	$.ajax({
-		type: "get",
-		url: getContextRootPath() + "login/api/sessionAvailable",
-		dataType: "json"
-	}).done(function(result) {
-		var isAvailable = result["available"] == true;
-		console.log("result-available:", isAvailable);
-		if (isAvailable) {
-			okAction();
-		} else {
-			ngAction();
-		}
-	}).fail(function(result, status) {
-		console.log("failed: result.status,status=", result.status, status);
-		errorAction("ajax-fail: r.s=" + result.status + ",st=" + status);
-	});
-}
-
 // inject events
 $(function() {
 	// version label - scroll to top
